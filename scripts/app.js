b4b5302cc648aeafce6c2db5e44afc76e28c50b0
brittonhayes <46035482+brittonhayes@users.noreply.github.com>
2019-01-02 23:36:30 -0800

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -188,8 +188,8 @@ document.addEventListener('click',
     // console.log('hello world');
     if (e.target.parentElement.classList.contains('check-item')) {
       e.target.parentElement.parentElement.classList.toggle('checked'),
-        e.target.classList.toggle('white-text'),
-        e.target.parentElement.innerText = "COMPLETE"
+        e.target.classList.toggle('white-text')
+        // e.target.parentElement.innerText = "COMPLETE"
         // document.getElementsByClassName('delete-item').classList.toggle('white-text'),
         // console.log(e.target.nextSibling)
     }
